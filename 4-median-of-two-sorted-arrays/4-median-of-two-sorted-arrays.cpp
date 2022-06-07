b6c5ca1b933378bf65e36a0d8b70c853da1a3c77
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2022-06-07 20:00:53 +0530

    Time: 46 ms (72.15%), Space: 89.2 MB (97.37%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-   double mediann(vector<int>&a,vector<int>&b){
-        int m=a.size();
-        int n=b.size();
-        if(m>n)
-            return mediann(b,a);
-        int l=0,r=m;
-        while(l<=r){
-            int partx=l+(r-l)/2;
-            int party=(m+n+1)/2-partx;
-            int maxlx=(partx==0)?INT_MIN:a[partx-1];
-            int minrx=(partx==m)?INT_MAX:a[partx];
-            int maxly=(party==0)?INT_MIN:b[party-1];
-            int minry=(party==n)?INT_MAX:b[party];
-            if(maxlx<=minry&&maxly<=minrx){
-                if((m+n)%2==0)
-                    return (double)(max(maxlx,maxly)+min(minrx,minry))/2;
-                else
-                    return (double)(max(maxlx,maxly));
-            }else if(maxlx>minry)
-                r=partx-1;
-            else
-                l=partx+1;
-        }
-        return -1.0;
+    double findMedianSortedArrays(vector<int> &a1, vector<int> &a2)
+{
+    if (a1.size() > a2.size())
+        swap(a1, a2); // make sure a1 is shorter
+
+    int n1 = a1.size(), n2 = a2.size();
+
+    // range of a1 cut location: n1 means no right half for a1
+    int lo = 0, hi = n1;
+    while (lo <= hi)
+    {
+        int cut1 = (lo + hi) / 2; // cut location is counted to right half
+        int cut2 = (n1 + n2) / 2 - cut1;
+
+        int l1 = cut1 == 0 ? INT32_MIN : a1[cut1 - 1];
+        int l2 = cut2 == 0 ? INT32_MIN : a2[cut2 - 1];
+        int r1 = cut1 == n1 ? INT32_MAX : a1[cut1];
+        int r2 = cut2 == n2 ? INT32_MAX : a2[cut2];
+
+        if (l1 > r2)
+            hi = cut1 - 1;
+        else if (l2 > r1)
+            lo = cut1 + 1;
+        else
+            return (n1 + n2) % 2 ? min(r1, r2) : (max(l1, l2) + min(r1, r2)) / 2.;
     }
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double ans;
-        ans=mediann(nums1,nums2);
-        return ans;   
-    }
+    return -1;
+}
 };
