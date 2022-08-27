03483b62ede540ef6ab2807dab67eb2773e0c429
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2022-08-27 17:41:41 +0530

    Time: 210 ms (9.23%), Space: 21 MB (5.57%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -7,6 +7,7 @@ public:
         int res = 0;
         for(int i = 0; i < matrix.size(); ++i) {
             for(int j = 0; j < matrix[0].size(); ++j) {
+                 if (matrix[i][j] == '1')
                 res = max(res, solve(matrix, mem, i, j));
             }
         }
