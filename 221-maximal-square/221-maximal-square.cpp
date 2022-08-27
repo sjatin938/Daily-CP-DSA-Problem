137275aed56f49247537c8a248a3e70074f1bc99
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2022-08-27 18:29:21 +0530

    Time: 102 ms (53.39%), Space: 20.9 MB (5.57%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
-   int helper(vector<vector<char>>& matrix, int row, int col, vector<vector<int>> &dp) {
-
-    if (row < 0 || col < 0 || row >= matrix.size() || col >= matrix[0].size() || matrix[row][col] == '0') {
-        return 0;
-    }
-    if (dp[row][col] != -1) {
-
-        return dp[row][col];
-    }
-    int left = helper(matrix, row, col - 1, dp);
-    int up = helper(matrix, row - 1, col, dp);
-    int diag = helper(matrix, row - 1, col - 1, dp);
-    int ans = min(left, min(up, diag)) + 1;
-    return dp[row][col] = ans;
-}
-
-int maximalSquare(vector<vector<char>> &matrix) {
-    if (matrix.size() == 0) {
-        return 0;
-    }
-    int m = matrix.size();
-    int n = matrix[0].size();
-
-    vector<vector<int>> dp(m, vector<int>(n, -1));
-    int maxM = 0;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] == '1') {
-                int ans = helper(matrix, i, j, dp);
-                maxM = max(ans, maxM);
-            }
-
+  int maximalSquare(vector<vector<char>>& matrix) {
+    int m = matrix.size(), n = matrix[0].size(), res = 0;
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            if(!i || !j || matrix[i][j] == '0')
+                dp[i][j] = matrix[i][j] - '0';
+            else
+                dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
+            res = max(res, dp[i][j] * dp[i][j]);
         }
     }
-    return maxM * maxM;
+    return res;
 }
 };
