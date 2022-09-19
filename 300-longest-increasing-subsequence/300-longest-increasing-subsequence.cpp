0db534cd259ba3165df97e5b579845839b77115c
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2022-09-19 22:25:42 +0530

    Time: 2886 ms (5.00%), Space: 292.2 MB (8.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-        
-    int recursionM(int ind,int prev_ind,vector<int>& nums,vector<vector<int>>& dp){
-        if(ind==nums.size()) return 0;
-        
-        if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
-        
-        // not take
-        int notTake=recursionM(ind+1,prev_ind,nums,dp);
-        
-        //take
-        int take=INT_MIN;
-        if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-            take=1+recursionM(ind+1,ind,nums,dp);
-        }
-        
-        return dp[ind][prev_ind+1]=max(take,notTake);
+    int lengthOfLIS(vector<int> &nums, int idx, int prev, vector<vector<int>> &dp) {
+    if (idx == nums.size()) {
+        return 0;
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        //return recursion(0,-1,nums); // Recursion
-        return recursionM(0,-1,nums,dp);
+    if (dp[idx][prev + 1] != -1) {
+        return dp[idx][prev + 1];
     }
+    int take = INT32_MIN;
+    if (prev == -1 || nums[idx] > nums[prev]) {
+        take = 1 + lengthOfLIS(nums, idx + 1, idx, dp);
+    }
+    int dontTake = lengthOfLIS(nums, idx + 1, prev, dp);
+    return dp[idx][prev + 1] = max(take, dontTake);
+}
+
+int lengthOfLIS(vector<int> &nums) {
+    vector<vector<int>> dp(nums.size(), vector<int>(nums.size() + 1, -1));
+    return lengthOfLIS(nums, 0, -1, dp);
+}
+
 };
