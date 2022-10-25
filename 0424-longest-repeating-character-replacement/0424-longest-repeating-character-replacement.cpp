939b4ca5b1f706777237b0be575f15865e325164
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2022-10-25 11:22:44 +0530

    Time: 24 ms (48.46%), Space: 7.1 MB (13.21%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,14 +4,14 @@ public:
     // k =  non-SameCharacter
     int len = 0;
     int sameCharacter = 0;
-    int j = 0;
+    int startIndx = 0;
     unordered_map<int, int> mp;
     for (int i = 0; i < s.length(); i++) {
         sameCharacter = max(sameCharacter, ++mp[s[i]]);
         if (len - sameCharacter < k) {
             len++;
         } else {
-            mp[s[j++]]--;
+            mp[s[startIndx++]]--;
         }
     }
     return len;
