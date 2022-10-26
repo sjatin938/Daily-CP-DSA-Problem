e6587b8e343c69dcb6fdfdab409b223930e0df9e
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2022-10-26 13:05:27 +0530

    Time: 633 ms (18.84%), Space: 22.5 MB (40.56%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-    vector <vector<bool>> palin(s.length(), vector<bool>(s.length(), 0));
+    vector<vector<bool>> palin(s.length(), vector<bool>(s.length(), 0));
     int idx = -1, len = -1;
     for (int diag = 0; diag < s.length(); diag++) {
         for (int row = 0, col = diag; col < s.length(); row++, col++) {
             if (s[row] == s[col])
                 if (diag == 0 || diag == 1 || palin[row + 1][col - 1] == 1)
                     palin[row][col] = 1;
-            
             if (palin[row][col] == 1) {
                 idx = row;
                 len = diag + 1;
