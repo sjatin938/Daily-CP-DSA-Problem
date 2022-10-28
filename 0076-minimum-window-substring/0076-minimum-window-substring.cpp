ddefffc4380dc7484484cae3a10bdfdac7e25b40
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2022-10-28 19:43:11 +0530

    Time: 19 ms (74.18%), Space: 8 MB (30.45%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,47 +1,39 @@
-class Solution
-{
-    public:
-        string minWindow(string s, string t)
-        {
-            if (s.length() == 0 || t.length() == 0)
-                return "";
+class Solution {
+public:
+  
+string minWindow(string s, string t) {
+    if (s.length() < t.length())
+        return "";
 
-            vector<int> mp(123, 0);
-            for (auto &x: t)
-                mp[x]++;
+    vector<int> count(123, 0);
+    for (auto &x: t)
+        count[x]++;
 
-            int maxLen = INT32_MAX;
-            int start = 0;
-            int left = 0;
-            int len = t.length();
-            for (int i = 0; i <= s.length(); i++)
-            {
-
-                if (len)
-                {
-                    if (i == s.size())
-                        break;
-                    mp[s[i]]--;
-                    if (mp[s[i]] >= 0)
-                    {
-                        len--;
-                    }
-                }
-                else
-                {
-
-                    if (i - start < maxLen)
-                    {
-                        maxLen = i - start;
-                        left = start;
-                    }
-                    mp[s[start]]++;
-                    if (mp[s[start]] > 0)
-                        len++;
-                    start++;
-                    i--;
-                }
+    int len = t.length();
+    int minLen = INT32_MAX;
+    int start = 0;
+    int idx = 0;
+    for (int i = 0; i <= s.length(); i++) {
+        if (len == 0) {
+            count[s[start]]++;
+            if (minLen > i - start) {
+                minLen = i - start;
+                idx = start;
             }
-            return maxLen == INT32_MAX ? "" : s.substr(left, maxLen);
+            if (count[s[start]] > 0) {
+                len++;
+            }
+              i--;
+            start++;
+        } else {
+            if (i == s.length())
+                break;
+            count[s[i]]--;
+            if (count[s[i]] >= 0) {
+                len--;
+            }
         }
+    }
+    return  minLen == INT32_MAX ? "" : s.substr(idx, minLen);
+}
 };
