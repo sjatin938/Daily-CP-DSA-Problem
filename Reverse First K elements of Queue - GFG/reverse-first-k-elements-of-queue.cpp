7d5a6ad0c03c5107641c35530d1263d6f8d03136
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2022-11-09 21:50:49 +0530

    Added solution - LeetHub

---
+++ Reverse First K elements of Queue - GFG/reverse-first-k-elements-of-queue.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+queue<int> modifyQueue(queue<int> q, int k);
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n, k;
+        cin >> n >> k;
+        queue<int> q;
+        while (n-- > 0) {
+            int a;
+            cin >> a;
+            q.push(a);
+        }
+        queue<int> ans = modifyQueue(q, k);
+        while (!ans.empty()) {
+            int a = ans.front();
+            ans.pop();
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for C++
+
+// Function to reverse first k elements of a queue.
+queue<int> modifyQueue(queue<int> q, int k) {
+    vector<int> rev;
+    while (!q.empty()) {
+        rev.push_back(q.front());
+        q.pop();
+    }
+    reverse(rev.begin(), rev.begin() + k);
+    for (int &i: rev) {
+        q.push(i);
+    }
+    return q;
+}
