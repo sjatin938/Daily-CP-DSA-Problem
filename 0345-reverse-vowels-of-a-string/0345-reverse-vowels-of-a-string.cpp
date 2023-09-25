ec8d819fd5549887e7d069688d4a225c537c4306
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2023-09-25 11:54:03 +0530

    Time: 3 ms (96.19%), Space: 7.8 MB (47.43%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+  string reverseVowels(string s)
+    {
+        int i = s.length() - 1;
+        int j = 0;
+        while (j < i)
+        {
+            if ((s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u' &&
+                 s[i] != 'A' && s[i] != 'E' && s[i] != 'I' && s[i] != 'O' && s[i] != 'U'))
+            {
+                i--;
+            }
+            if (s[j] != 'a' && s[j] != 'e' && s[j] != 'i' && s[j] != 'o' && s[j] != 'u' &&
+                s[j] != 'A' && s[j] != 'E' && s[j] != 'I' && s[j] != 'O' && s[j] != 'U')
+            {
+                j++;
+            }
+
+            if ((s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+                 s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') &&
+                (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' ||
+                 s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U'))
+            {
+                char c = s[i];
+                s[i] = s[j];
+                s[j] = c;
+                i--;
+                j++;
+            }
+        }
+        return s;
+    }
+};
