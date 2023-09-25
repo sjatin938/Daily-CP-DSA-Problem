3739e72ae97ddaeafd4d590cbbbd631c04d97579
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2023-09-25 13:15:32 +0530

    Time: 19 ms (54.94%), Space: 25 MB (43.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,24 +1,23 @@
-class Solution
-{
-    public:
-        vector<int> productExceptSelf(vector<int> &nums)
-        {
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        if(nums.size()<2)return nums;
 
-            vector<int> left(nums.size(), 1);
-            vector<int> right(nums.size(), 1);
+        vector<int>left(nums.size(), 1);
+        vector<int>right(nums.size() ,1);
 
-            for (int i = 1; i < nums.size(); i++)
-            {
-                left[i] = left[i - 1] *nums[i - 1];
-            }
-            for (int j = nums.size() - 2; j >= 0; j--)
-            {
-                right[j] = right[j + 1] *nums[j + 1];
-            }
-            for (int i = 0; i < nums.size(); i++)
-            {
-                nums[i] = left[i] *right[i];
-            }
-            return nums;
+        for(int i =1; i < nums.size(); i++){
+            left[i] = nums[i-1] *left[i-1];
         }
-        };
+
+        for(int i =nums.size()-2 ;i >=0; i--){
+            right[i] = right[i+1]*nums[i+1];
+        }
+
+        for(int i =0 ; i < nums.size() ;i ++){
+            left[i] = left[i]*right[i];
+        }
+
+return left;
+    }
+};
