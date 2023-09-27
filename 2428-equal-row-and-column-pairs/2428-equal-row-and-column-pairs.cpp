baf76810db601030ebd5b5a1e1e8f9bbb73a8195
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2023-09-27 16:19:25 +0530

    Time: 59 ms (95.91%), Space: 33 MB (48.48%) - LeetHub

---
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+
+   // unordered_map does not support map;
+
+    int equalPairs(vector<vector<int>> &grid) {
+    map<vector<int>, int> m;
+    int n = grid.size();
+    for (int i = 0; i < n; i++) {
+        m[grid[i]]++;
+    }
+    int count = 0;
+    for (int c = 0; c < n; c++) {
+        vector<int> temp;
+        for (int i = 0; i < n; i++) {
+            temp.push_back(grid[i][c]);
+        }
+        count += m[temp];
+    }
+    return count;
+}
+};
