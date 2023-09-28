98690a2d0a71710e8f168e11649280f6015a3c53
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2023-09-28 22:49:09 +0530

    Time: 0 ms (100%), Space: 7.1 MB (7.87%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-   vector<string> s{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-vector<string> ans;
-
-void helper(string digits, string res, int i) {
-    if (i == digits.length()) {
+    vector<string> temp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> ans;
+    void helper(string digits , string &res , int i){
+        if(i== digits.length()){
         ans.push_back(res);
-        return;
-    }
-
-    for (int j = 0; j < s[digits[i] - '0'].length(); j++) {
-        res.push_back(s[digits[i] - '0'][j]);
-        helper(digits, res, i + 1);
-        res.pop_back();
-    }
+        return; 
+        }
+        int t = digits[i]-'0';
+        for(int j =0 ; j < temp[t].length(); j++){
+            char c  = temp[t][j];
+            res = res+c;
+            helper(digits , res , i+1);
+            res.pop_back();
+        }
 }
+    vector<string> letterCombinations(string digits) {
+       if(digits.length()==0)
+       return {};
 
-vector<string> letterCombinations(string digits) {
-     if (digits == "")
-     return {};
-    string res = "";
-    helper(digits, res, 0);
+    string res;
+    helper(digits , res , 0); 
     return ans;
-}
+    }
 };
