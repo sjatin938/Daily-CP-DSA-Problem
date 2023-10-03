623dcd132f635b2bd9862eebf2f0e0b9ba97ba04
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2023-10-03 13:59:43 +0530

    Time: 99 ms (17.07%), Space: 11.6 MB (66.01%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,37 +1,33 @@
-class Solution
-{
-    public:
-        bool isMatch(string &s, string &p, int i, int j, vector<vector < int>> &dp)
-        {
-
-            if (i < 0 && j < 0)
-                return true;
-
-            if (i < 0 && j >= 0)
-            {
-                for (int k = 0; k <= j; k++)
-                    if (p[k] != '*')
-                        return false;
-
-                return true;
-            }
-            if (i >= 0 && j < 0)
-                return false;
-            if (dp[i][j] != -1)
-                return dp[i][j];
-            if (s[i] == p[j] || p[j] == '?')
-                return dp[i][j] = isMatch(s, p, i - 1, j - 1, dp);
-            else if (p[j] == '*')
-            {
-                return dp[i][j] = isMatch(s, p, i - 1, j, dp) || isMatch(s, p, i, j - 1, dp);
-            }
-            else
-                return dp[i][j] = false;
-        }
-
-    bool isMatch(string s, string p)
-    {
-        vector<vector < int>> dp(s.length(), vector<int> (p.length(), -1));
-        return isMatch(s, p, s.length() - 1, p.length() - 1, dp);
+class Solution {
+public:
+   bool isMatch(std::string s, std::string p) {
+    int m = s.length();
+    int n = p.length();
+    
+    // Initialize a 2D vector to store the matching status
+    std::vector<std::vector<bool>> dp(m + 1, std::vector<bool>(n + 1, false));
+    
+    // Empty pattern matches an empty string
+    dp[0][0] = true;
+    
+    // Handle patterns with '*' as the first character
+    for (int j = 1; j <= n; ++j) {
+        if (p[j - 1] == '*')
+            dp[0][j] = dp[0][j - 1];
     }
+    
+    // Fill the dp table based on the pattern and string
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (p[j - 1] == '*') {
+                dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+            } else if (p[j - 1] == '?' || p[j - 1] == s[i - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+        }
+    }
+    
+    // Return true if the entire string matches the pattern
+    return dp[m][n];
+}
 };
