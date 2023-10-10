ee3e6ef55eed722f96060ce010beccb711a8d486
Jatin Sharma <75081474+sjatin938@users.noreply.github.com>
2023-10-10 14:39:28 +0530

    Time: 99 ms (19.02%), Space: 11.6 MB (64.86%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-   bool isMatch(std::string s, std::string p) {
+   bool isMatch(const std::string& s, const std::string& p) {
     int m = s.length();
     int n = p.length();
-    
-    // Initialize a 2D vector to store the matching status
+
+    // Initialize a dynamic programming table
     std::vector<std::vector<bool>> dp(m + 1, std::vector<bool>(n + 1, false));
-    
-    // Empty pattern matches an empty string
     dp[0][0] = true;
-    
-    // Handle patterns with '*' as the first character
+
+    // Handle the '*' in the pattern
     for (int j = 1; j <= n; ++j) {
         if (p[j - 1] == '*')
             dp[0][j] = dp[0][j - 1];
     }
-    
-    // Fill the dp table based on the pattern and string
+
+    // Fill the DP table
     for (int i = 1; i <= m; ++i) {
         for (int j = 1; j <= n; ++j) {
-            if (p[j - 1] == '*') {
+            if (p[j - 1] == '*')
                 dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-            } else if (p[j - 1] == '?' || p[j - 1] == s[i - 1]) {
+            else if (p[j - 1] == '?' || p[j - 1] == s[i - 1])
                 dp[i][j] = dp[i - 1][j - 1];
-            }
         }
     }
-    
-    // Return true if the entire string matches the pattern
+
     return dp[m][n];
 }
+
 };
